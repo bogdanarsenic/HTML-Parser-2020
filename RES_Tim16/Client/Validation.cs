182aedd36008c788a4721c3c8cac695f179c7b6e
Dejan Bozic <dejan.bozic.94@gmail.com>
2020-05-25 14:26:38 +0200

    Adding CheckIfStringEmpty

---
+++ RES_Tim16/Client/Validation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class Validation : IValidation
+    {
+
+        public bool CheckIfStringEmpty(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                Console.WriteLine("String is NULL or empty!");
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
