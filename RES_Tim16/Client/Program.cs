c5bd6cab041f133eb12a14cab04fa5276955a1b2
Bogdan Arsenic <bogdanarsenic@gmail.com>
2020-05-25 18:39:18 +0200

    Checking File content

--- RES_Tim16/Client/Program.cs
+++ RES_Tim16/Client/Program.cs
@@ -15,37 +15,59 @@ namespace Client
 
             while (option == "")
             {
-
+                Console.WriteLine("-----MENU-----");
                 Console.WriteLine("Choose an option from menu: ");
                 Console.WriteLine("1. Insert file path");
                 Console.WriteLine("2. Write html directly");
                 Console.WriteLine("If you want to exit - click X");
-
+                Console.WriteLine("--------------");
                 option = Console.ReadLine();
 
                 switch (option)
                 {
                     case "1":
+
+                        IValidation validate = new Validation();
                         Console.WriteLine("Insert file path:");
                         option = Console.ReadLine();
-                        IValidation validate = new Validation();
-
+                        
                         if(!validate.CheckIfStringEmpty(option) && validate.CheckIfPathCorrect(option))
                         {
                             //Proslediti parseru
+                            Console.ReadLine();
                         }
-                        
+
+                        option = "";
                         break;
+
                     case "2":
 
+                        IValidation validateOpt2 = new Validation();
                         Console.WriteLine("Write the filename:");
                         option = Console.ReadLine();
-                        Console.WriteLine("Write the file content:");
+                        bool nameEmpty=validateOpt2.CheckIfNameEmpty(option);
 
-                        
+                        if(nameEmpty)
+                        {
+                            option = "";
+                            break;
+                        }
+
+                        Console.WriteLine("Write the file content:");
+                        textcontent = Console.ReadLine();
+                        bool contentEmpty=validateOpt2.CheckIfContentEmpty(textcontent);
+
+                        if(contentEmpty)
+                        {
+                            option = "";
+                            break;
+                        }
+
+                        //Prosledjuje se parseru.
+                        option = "";
                         break;
                     case "X":
-
+                        option = "x";
                         break;
                     default:
                         option = "";
