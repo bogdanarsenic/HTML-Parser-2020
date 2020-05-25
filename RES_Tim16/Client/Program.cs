7da61e4731af899c1675043afe6e39d1415b469a
Bogdan Arsenic <bogdanarsenic@gmail.com>
2020-05-25 14:53:21 +0200

    Adding Menu

--- RES_Tim16/Client/Program.cs
+++ RES_Tim16/Client/Program.cs
@@ -10,7 +10,39 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Bogdane!");
+            string option = "";
+
+            while (option == "")
+            {
+
+
+                Console.WriteLine("Choose an option from menu: ");
+                Console.WriteLine("1. Insert file path");
+                Console.WriteLine("2. Write html directly");
+
+                option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "1":
+                        Console.WriteLine("Insert file path:");
+                        option = Console.ReadLine();
+                        break;
+                    case "2":
+                        Console.WriteLine("Write the filename:");
+                        option = Console.ReadLine();
+                        Console.WriteLine("Write the file content:");
+                        break;
+                    default:
+                        option = "";
+                        Console.WriteLine("You need to put 1 or 2");
+                        break;
+                }
+            }
+
+            Console.WriteLine("Enter for exit");
+            Console.ReadLine();
         }
+
     }
 }
