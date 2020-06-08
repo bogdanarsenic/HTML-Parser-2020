769fad26707a7b02f6b570e70644f2af410f8e19
Dejan Bozic <dejan.bozic.94@gmail.com>
2020-06-09 01:40:10 +0200

    Adding UIController

---
+++ RES_Tim16/UIController/Controller.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UIControllerMiddleware;
+
+namespace UIController
+{
+    public class Controller : IController
+    {
+        public string Name { get; set; }
+        public string Content { get; set; }
+        public string DeltaContent { get; set; }
+        public string LineRange { get; set; }
+        public string DatabaseContent { get; set; }
+
+        public Controller()
+        {
+
+        }
+
+        public Controller(string name, string content)
+        {
+            if (name == null || content == null)
+            {
+                throw new ArgumentException("Properties can't be null!");
+            }
+
+            Name = name;
+            Content = content;
+
+            GetFileInformation();
+        }
+
+        public string GetFileInformation()
+        {
+           return Name + '|' + Content;
+        }
+
+        public string ReceiveDeltaInformation()
+        {
+            return DeltaContent + '|' + LineRange + '|' + DatabaseContent;
+        }
+
+        public void SendDeltaInformation(string textcontent, string lineRange, string databaseContent)
+        {
+            DeltaContent = textcontent;
+            LineRange = lineRange;
+            DatabaseContent = databaseContent;
+            ReceiveDeltaInformation();
+        }
+    }
+}
