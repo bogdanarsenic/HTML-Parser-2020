a01cd7db178f23b5d188441cf0dde172a4b17f68
Dejan Bozic <dejan.bozic.94@gmail.com>
2020-06-09 01:19:56 +0200

    Make UIControllerMiddleware

---
+++ RES_Tim16/UIControllerMiddleware/IController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UIControllerMiddleware
+{
+    public interface IController : IControllerForUI
+    {
+        string Name { get; set; }
+        string Content { get; set; }
+        string DeltaContent { get; set; }
+        string LineRange { get; set; }
+        string DatabaseContent { get; set; }
+
+        string GetFileInformation();
+
+        void SendDeltaInformation(string name, string content, string databaseContent);
+    }
+}
