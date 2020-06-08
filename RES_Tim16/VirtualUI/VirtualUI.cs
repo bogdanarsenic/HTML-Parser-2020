e53a235d3ea5f6cacf439672627927f65c96ad2e
Bogdan Arsenic <bogdanarsenic@gmail.com>
2020-06-09 00:47:25 +0200

    Parsing information from Controller

---
+++ RES_Tim16/VirtualUI/VirtualUI.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualUI.Models;
+
+namespace VirtualUI
+{
+    public class VirtualUI
+    {
+
+        private string Name { get; set; }
+        private string Content { get; set; }
+
+        public VirtualUI()
+        {
+  
+
+            ParseInformationFromController(Name, Content);
+
+        }
+
+        public void ParseInformationFromController(string name, string content)
+        {
+           
+
+            Files file = new Files();
+            file.Name = name.Split('.')[0];
+            file.Extension = name.Split('.')[1];
+            file.Id = name;
+
+            FileContent fileContent = new FileContent();
+            fileContent.FileId = file.Id;
+            fileContent.Content = content;
+
+        }
+
+    }
+}
