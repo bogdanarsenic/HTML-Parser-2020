d35b12ad1bef225d982a9b4cd1bb3630be6bdd77
Dejan Bozic <dejan.bozic.94@gmail.com>
2020-06-11 12:40:31 +0200

    Adding IParser interface

---
+++ RES_Tim16/Parser/IParser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser
+{
+    public interface IParser
+    {
+        bool Check(string content);
+        void AllowedTagsList(List<string> htmlTags);
+        bool CheckHtmlStartTagsUntilTitle(string text);
+        bool CheckHtmlTagsAfterTitleUntilBody(string text);
+        bool CheckHtmlTagsAfterBodyUntilEnd(string text);
+        bool CheckHtmlTagsNotInsideBody(string text);
+        List<string> SplitHtmlText(string text);
+        bool CheckHtmlTagsInsideBody(string text);
+        bool CheckIfValidTag(List<string> list, string tag);
+
+    }
+}
---
+++ RES_Tim16/Parser/Parser.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser
+{
+    public class Parser : IParser
+    {
+        public void AllowedTagsList(List<string> htmlTags)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Check(string content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckHtmlStartTagsUntilTitle(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckHtmlTagsAfterBodyUntilEnd(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckHtmlTagsAfterTitleUntilBody(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckHtmlTagsInsideBody(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckHtmlTagsNotInsideBody(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckIfValidTag(List<string> list, string tag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> SplitHtmlText(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
