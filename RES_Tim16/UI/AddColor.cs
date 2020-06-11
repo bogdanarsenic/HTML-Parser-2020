2b0fb2b14f181aff720763bea87fc5369ebf9a1d
Dejan Bozic <dejan.bozic.94@gmail.com>
2020-06-11 14:45:38 +0200

    Add method for adding color in text that is different

--- RES_Tim16/UI/AddColor.cs
+++ RES_Tim16/UI/AddColor.cs
@@ -28,7 +28,47 @@ namespace UI
             Console.WriteLine("Comparing old and new file...");
             Console.WriteLine("New file content is: ");
 
+            AddDifference(previous, newContents, delta, numLines, lines);
+        }
 
+        public void AddDifference(string[] databaseText, string[] newText, string[] delta, int numberOfLines, int[] lines)
+        {
+            int y = 0;
+            for (int i = 1; i <= numberOfLines; i++)
+            {
+
+                if (lines.Contains(i))
+                {
+                    if (databaseText.Length >= i)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        Console.WriteLine("-" + i + "." + databaseText[i - 1]);
+
+                    }
+
+                    if (delta[y] == "")
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        y++;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("+" + i + "." + newText[i - 1]);
+
+                        y++;
+                    }
+
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("*" + i + "." + newText[i - 1]);
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
