992912207adc645bac9c600969d47b7ba405e159
Bogdan Arsenic <bogdanarsenic@gmail.com>
2020-06-15 17:29:17 +0200

    Making Menu in Client

---
+++ RES_Tim16/Client/Menu.cs
@@ -0,0 +1,68 @@
+﻿using Parser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class Menu
+    {
+        public IValidation validate;
+        public string key;
+
+        public void Print()
+        {
+            do
+            {
+                Console.WriteLine("-----MENU-----");
+                Console.WriteLine("Choose an option from menu: ");
+                Console.WriteLine("1. Insert file name");
+                Console.WriteLine("2. Write html directly");
+                Console.WriteLine("If you want to exit - click X");
+                Console.WriteLine("--------------");
+
+                key = Console.ReadLine();
+
+            } while (key != "1" && key != "2" && key != "x" && key != "X");
+
+
+            switch(key)
+            {
+                case "1":
+                            Console.WriteLine("Insert file name:");
+                            string nameFile = Console.ReadLine();
+
+                            if (validate.CheckIfPathCorrect(nameFile) && !validate.CheckIfStringEmpty(nameFile))
+                            {
+                                IFileParser p1 = new FileParser();
+                                p1.OpenExistingFileForParsing(nameFile);
+                                
+                                
+                                
+                            }
+
+                            break;
+                case "2":
+                            Console.WriteLine("Write file name:");
+                            string nameFile2 = Console.ReadLine();
+
+                            if(validate.CheckIfPathCorrect(nameFile2) || validate.CheckIfStringEmpty(nameFile2))
+                            {
+                                
+                            }
+                            Console.WriteLine("Write file content:");
+                            string fileContent = Console.ReadLine();
+
+                    break;
+                case "x":
+                    break;
+                case "X":
+                    break;
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+    }
+}
--- RES_Tim16/Client/Program.cs
+++ RES_Tim16/Client/Program.cs
@@ -14,96 +14,9 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            string option = "";
-            string textcontent="";
-            bool input = false;
 
-            IValidation validate = new Validation();
-
-
-            while (!input)
-            {
-                Console.WriteLine("-----MENU-----");
-                Console.WriteLine("Choose an option from menu: ");
-                Console.WriteLine("1. Insert file path");
-                Console.WriteLine("2. Write html directly");
-                Console.WriteLine("If you want to exit - click X");
-                Console.WriteLine("--------------");
-                option = Console.ReadLine();
-
-                switch (option)
-                {
-                    case "1": 
-
-                        Console.WriteLine("Insert file path:");
-                        string path = Console.ReadLine();
-                        
-                        if(!validate.CheckIfStringEmpty(path) && validate.CheckIfPathCorrect(path))
-                        {
-                            //Forward to parser
-                            FileParser fp1 = new FileParser();
-                            string checkIfValidFileContent1 = "";
-                            checkIfValidFileContent1 = fp1.OpenExistingFileForParsing(path);
-
-                            if (!checkIfValidFileContent1.Equals("INVALID!"))
-                            {
-                                IController uIController1 = new Controller(path, checkIfValidFileContent1);
-                                VirtualUI.VirtualUI virtualUI1 = new VirtualUI.VirtualUI(uIController1);
-                                UIClass ui = new UIClass(uIController1);
-                            }
-
-                            input = false;
-                            Console.ReadLine();
-                        }
-
-                        break;
-
-                    case "2":
-
-                        Console.WriteLine("Write the filename:");
-                        string fileName = Console.ReadLine();
-                        bool nameEmpty = validate.CheckIfStringEmpty(fileName);
-
-                        if(nameEmpty || validate.CheckIfPathCorrect(fileName))
-                        {
-                            Console.WriteLine("Choose another name!");
-                            break;
-                        }
-
-                        Console.WriteLine("Write the file content:");
-                        textcontent = Console.ReadLine();
-                        bool contentEmpty=validate.CheckIfStringEmpty(textcontent);
-
-                        if(contentEmpty)
-                        {
-                            break;
-                        }
-
-                        //Forward to parser
-                        FileParser fp2 = new FileParser();
-                        fp2.CreateNewFileForParsing(fileName, textcontent);
-
-                        string checkIfValidFileContent2 = "";
-                        checkIfValidFileContent2 = fp2.OpenExistingFileForParsing(fileName);
-
-                        if (!checkIfValidFileContent2.Equals("INVALID!"))
-                        {
-                            IController uIController2 = new Controller(fileName, checkIfValidFileContent2);
-                            VirtualUI.VirtualUI virtualUI2 = new VirtualUI.VirtualUI(uIController2);
-                            UIClass ui = new UIClass(uIController2);
-
-                        }
-
-                        input = false;
-                        break;
-                    case "X":
-                        input = true;
-                        break;
-                    default:
-                        Console.WriteLine("You need to put 1 or 2");
-                        break;
-                }
-            }
+            Menu menu = new Menu();
+            menu.Print();
 
         }
 
